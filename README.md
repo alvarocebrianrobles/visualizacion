1814969ee139b1c2e0e95b32f584d44e43d832b0
Álvaro Cebrián <alvarocebrian@MacBook-Pro-de-Alvaro.local>
2018-03-14 21:32:09 +0100

    Envío de prueba

--- README.md
+++ README.md
@@ -1,1 +1,108 @@
-# visualizacion
+# 01 - Mandatory Exercise
+
+Taking the "barchart refactor" sample that can be downloaded from here:
+
+https://github.com/Lemoncode/d3js-samples
+
+The goal of this exercise is:
+
+-  Add some space between columns.
+
+-  Add colors to each bar.
+
+-  Add a legend.
+
+-  Show the chart vertically.
+
+**Step by step solution**
+
+1. First part of the exercise can be done just subtracting some px from attribute 'width' when filling SVG with bars
+
+```javascript
+  .attr('width', y.bandwidth()-4)
+```
+
+2. Second part has been done by adding a new variable 'color' which stablishes them and adding a new attribute to the rectangle making function (bars painting):
+
+```javascript
+  var color = d3.scaleOrdinal()
+  .range(['green', 'red', 'blue']);
+
+newRects.append('rect')
+// here there are some others attributes
+  .attr("fill", function(d) { 
+    return color(d.product); 
+  });
+```
+
+3. Part three consists in add a new variable 'legend' to the rectangles making function (bars painting function) like shown below:
+
+```javascript
+    var legend = svg.selectAll(".legend")
+        .data(totalSales.slice())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
+    
+    legend.append("rect")
+        .attr("x", 520)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function (d, i) {
+            return color(d.product);
+        });
+    
+    legend.append("text")
+        .attr("x", 600)
+        .attr("y", 10)
+        .attr("dy", ".15em")
+        .style("text-anchor", "end")
+        .text(function (d) { return d.product; });
+```
+
+4. For last, in order to exchange X axis for Y axis is needed to make some adjustmens:
+
+    - Change scales config. Note that y range has to be changed for height-0, instead of 0-width.
+
+          ```javascript
+          function setupYScale()
+          {
+            var maxSales = d3.max(totalSales, function(d, i) {
+              return d.sales;
+            });
+
+            y = d3.scaleLinear()
+              .range([height, 0])
+              .domain([0, maxSales]);
+
+          }
+
+          function setupXScale()
+          {
+            x = d3.scaleBand()
+              .rangeRound([0, width-100])
+              .domain(totalSales.map(function(d, i) {
+                return d.product;
+              }));
+          }
+          ````
+
+    - Change x and y attributes
+
+          ```javascript
+          newRects.append('rect')
+            .attr('y', function(d) {
+              return y(d.sales);})
+            .attr('x', function(d, i) {
+              return x(d.product);
+            })
+          ```
+    
+    - Change width and height attributes
+          
+          ```javascript
+            .attr('width', x.bandwidth()-4)
+            .attr('height', function(d, i) {
+              return height - y(d.sales);
+            })
+          ```
--- main.js
+++ main.js
@@ -1,48 +1,122 @@
-var totalSales = [
-  { product: 'Hoodie', sales: 7 },
-  { product: 'Jacket', sales: 6 },
-  { product: 'Snuggie', sales: 9 },
-  ];
 
-var svg = d3.select('svg');
+// 1. Setting every variable to null. They are going to be change later with functions
+let margin = null,
+    width = null,
+    height = null;
 
-var rects = svg.selectAll('rect')
-  .data(totalSales);
+let svg = null;
+let x, y = null; 
 
-var maxSales = d3.max(totalSales, function(d, i) {
-  return d.sales;
-});
+setupCanvasSize();
+appendSvg("body");
+setupXScale();
+setupYScale();
+appendXAxis();
+appendYAxis();
+appendChartBars();
 
-var x = d3.scaleLinear()
-  .range([0, 350])
-  .domain([0, maxSales]);
+// 2. Setting up the size of the canvas and in which part of it is going to be working 
+function setupCanvasSize() {
+  margin = {top: 10, left: 80, bottom: 20, right: 30};
+  width = 700 - margin.left - margin.right;
+  height = 340 - margin.top - margin.bottom; //Change the height value
+}
 
-var y = d3.scaleBand()
-  .rangeRound([0, 75])
-  .domain(totalSales.map(function(d, i) {
-    return d.product;
-   }));
+function appendSvg(domElement) {
+  svg = d3.select(domElement).append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform",`translate(${margin.left}, ${margin.top})`);
+}
 
-var newRects = rects.enter();
+// 3. Setting up the axis scale
+    // It is necessary to change scale x and y when showing the chart vertically
+function setupYScale()
+{
+  var maxSales = d3.max(totalSales, function(d, i) {
+    return d.sales;
+  });
 
-var color = d3.scaleOrdinal()
+  y = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, maxSales]);
+
+}
+
+function setupXScale()
+{
+  x = d3.scaleBand()
+    .rangeRound([0, width-100])
+    .domain(totalSales.map(function(d, i) {
+      return d.product;
+    }));
+}
+
+// 4. Transforming axis and grouping SVG shapes together
+
+function appendXAxis() {
+  // Add the X Axis
+  svg.append("g")
+    .attr("transform",`translate(0, ${height})`)
+    .call(d3.axisBottom(x));
+}
+
+function appendYAxis() {
+  // Add the Y Axis
+  svg.append("g")
+  .call(d3.axisLeft(y));
+}
+
+// 5. Inserting bars into the SVG created before
+function appendChartBars()
+{
+    var rects = svg.selectAll('rect')
+    .data(totalSales);
+
+    // Now it's time to append to the list of Rectangles we already have
+    var newRects = rects.enter();
+
+    var color = d3.scaleOrdinal()
     .range(['green', 'red', 'blue']);
 
-newRects.append('rect')
-  .attr('x', x(0))
-  .attr('y', function(d, i) {
-    return y(d.product);
-  })
-  .attr('height', y.bandwidth()-2)
-  .attr('width', function(d, i) {
-    return x(d.sales);
-  })
-  .attr("fill", function(d) { 
-    return color(d.product); 
-    });
-
-function drawBarchChart(width, height) {
-  var svg = d3.select('svg');
-  drawBarchChart(svg._groups[0][0].clientWidth, svg._groups[0][0].clientHeight);
+    // Let's append a new Rectangles
+    newRects.append('rect')
+      .attr('y', function(d) {
+        return y(d.sales);})
+      .attr('x', function(d, i) {
+        return x(d.product);
+      })
+      // Changing bandwidth to add some space between columns
+      .attr('width', x.bandwidth()-4)
+      .attr('height', function(d, i) {
+        return height - y(d.sales);
+      })
+      .attr("fill", function(d) { 
+        return color(d.product); 
+        });
+    
+    var legend = svg.selectAll(".legend")
+        .data(totalSales.slice())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
+    
+    legend.append("rect")
+        .attr("x", 520)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function (d, i) {
+            return color(d.product);
+        });
+    
+    legend.append("text")
+        .attr("x", 600)
+        .attr("y", 10)
+        .attr("dy", ".15em")
+        .style("text-anchor", "end")
+        .text(function (d) { return d.product; });
 }
+
+
 
